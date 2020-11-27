6c85e8f061cb66280d34aaa0d24bc461febc29d5
nikitarizh <nrigworks@gmail.com>
2020-11-27 19:33:18 +0300

    feat: add date to console

--- src/main/java/com/nikitarizh/Console.java
+++ src/main/java/com/nikitarizh/Console.java
@@ -3,6 +3,10 @@ package com.nikitarizh;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;   
+
 public class Console {
 
     private TextFlow console;
@@ -11,29 +15,32 @@ public class Console {
     private String COLOR_GREEN = "#44cc00";
     private String COLOR_WARNING = "#ffc400";
     private String COLOR_RED = "#ff2e2e";
+
+    private DateTimeFormatter dtf;
     
     public Console(TextFlow c) {
         console = c;
+        dtf = DateTimeFormatter.ofPattern("dd.MM.yyy HH:mm:ss");  
     }
 
     public void logSuccess(String text) {
         Text t = new Text();
         t.setStyle("-fx-fill: " + COLOR_GREEN + ";-fx-font-weight:200;\n");
-        t.setText(text + "\n");
+        t.setText(dtf.format(LocalDateTime.now()) + " >>> " + text + "\n");
         pushText(t);
     }
 
     public void logWarning(String text) {
         Text t = new Text();
         t.setStyle("-fx-fill: " + COLOR_WARNING + ";-fx-font-weight:200;\n");
-        t.setText(text + "\n");
+        t.setText(dtf.format(LocalDateTime.now()) + " >>> " + text + "\n");
         pushText(t);
     }
 
     public void logError(String text) {
         Text t = new Text();
         t.setStyle("-fx-fill: " + COLOR_RED + ";-fx-font-weight:200;\n");
-        t.setText(text + "\n");
+        t.setText(dtf.format(LocalDateTime.now()) + " >>> " + text + "\n");
         pushText(t);
     }
 
